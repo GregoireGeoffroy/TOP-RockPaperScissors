93461ae1735dc8ec24a459ae95695f89bf3c22a5
GregoireGeoffroy <thegathering92@gmail.com>
2024-07-24 11:02:42 +0300

    Write logic for computer choice

---
+++ javascript.js
@@ -0,0 +1,22 @@
+// Logic to get the computer choice
+
+function getComputerChoice() {
+    const choices = ["rock", "paper", "scissors"];
+    const randomIndex = Math.floor(Math.random() * choices.length);
+    return choices[randomIndex];
+}
+
+// Logic to get the human choice
+
+
+
+// Declare the players score  variables
+
+
+
+// Logic to play a single round
+
+
+
+
+// Logic to play entire game
