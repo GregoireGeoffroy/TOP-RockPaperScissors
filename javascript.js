4fcff6ef9f655093d429cc76fa0d78f7c1bd814d
GregoireGeoffroy <thegathering92@gmail.com>
2024-07-24 11:15:08 +0300

    Add Human choice and scores variables

--- javascript.js
+++ javascript.js
@@ -1,5 +1,4 @@
 // Logic to get the computer choice
-
 function getComputerChoice() {
     const choices = ["rock", "paper", "scissors"];
     const randomIndex = Math.floor(Math.random() * choices.length);
@@ -7,8 +6,15 @@ function getComputerChoice() {
 }
 
 // Logic to get the human choice
+function getHumanChoice() {
+    let choice = prompt("Please enter your choice: rock, paper, or scissors").toLowerCase();
 
+    while (choice !== "rock" && choice !== "paper" && choice !== "scissors") {
+        choice = prompt("Invalid choice. Please enter rock, paper, or scissors").toLowerCase();
+    }
 
+    return choice;
+}
 
 // Declare the players score  variables
 
